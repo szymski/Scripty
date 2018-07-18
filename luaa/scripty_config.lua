3187f66b20e046705af8711f36dd0489b8aa3551
szymski <szymskipl@gmail.com>
2018-07-18 16:57:31 +0200

    Run graph in a coroutine

--- luaa/scripty_config.lua
+++ luaa/scripty_config.lua
@@ -1,7 +1,10 @@
 ScriptyConfig = { }
-local config = ScriptyConfig
+local Config = ScriptyConfig
 
 /*----------------------------  
     Scripty Configuration
 ------------------------------*/
 
+// Graphs will not take more than this percent of CPU time.
+// Don't set this too high - values close to 1 can freeze the server.
+Config.MaxCpuUsage = 0.5
